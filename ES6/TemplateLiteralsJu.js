b3862c564b30825cf3f62fac6d398bc63050a5ba
unknown <jrer25@gmail.com>
2021-12-02 22:42:37 -0500

    Commit Inicial

---
+++ ES6/TemplateLiteralsJu.js
@@ -0,0 +1,8 @@
+//ES6
+var firstname = '0';
+var lastname = 'Torres';
+var age = 10;
+// ? -> Entonces
+// : -> Sino
+console.log(`${firstname == '0' ? 'A' : 'B'} 
+Eneque   ${lastname}  ${age * 10}`);
